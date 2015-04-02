2822ff30d929c458b6928f31b95c0c4e69aab7be
Tobias Oetiker <tobi@oetiker.ch>
2015-04-02 17:57:38 +0200

    fix example

--- example/SourceyBuild.sh
+++ example/SourceyBuild.sh
@@ -1,5 +1,5 @@
 SOURCEY_VERBOSE=1
 SOURCEY_REBUILD=1
-#buildPerl 5.20.2
-#buildPerlModule Mojolicious
-buildAuto http://prdownloads.sourceforge.net/libpng/libpng-1.6.16.tar.xz
+buildPerl 5.20.2
+buildPerlModule Mojolicious
+#buildAuto http://prdownloads.sourceforge.net/libpng/libpng-1.6.16.tar.xz
