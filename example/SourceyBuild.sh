c7d0dcb7ad7edb3ac5eceb072d7b8db091bb5d2c
Tobias Oetiker <tobi@oetiker.ch>
2015-04-03 14:31:47 +0200

    travis testing added

--- example/SourceyBuild.sh
+++ example/SourceyBuild.sh
@@ -7,11 +7,11 @@
 # create a copy of perl
 buildPerl 5.20.2
 
-# build the Mojolicious perl module
-# this calles cpanm internally ... 
-buildPerlModule Mojolicious
-
 # and this one is entirely unrelated, and just here
 # to show how to build a library. we do not actually need
 # it for the example code in app-simple.pl to work
-buildAuto http://prdownloads.sourceforge.net/libpng/libpng-1.6.16.tar.xz
+buildAuto https://ftp.postgresql.org/pub/source/v9.4.1/postgresql-9.4.1.tar.bz2
+
+# build the Mojolicious perl module
+# this calles cpanm internally ... 
+buildPerlModule Mojolicious Mojo::Pg
